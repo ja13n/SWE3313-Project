14828f75e1995718a6e3a580bf88f6b326efdb79
jalen <jearly3@students.chattahoochetech.edu>
2024-04-19 18:30:57 -0400

    Changed a few of the settings. Added transitive controls

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module com.example {
     requires transitive javafx.graphics;
-    requires javafx.controls;
+    requires transitive javafx.controls;
     requires javafx.fxml;
     requires java.sql;
 
