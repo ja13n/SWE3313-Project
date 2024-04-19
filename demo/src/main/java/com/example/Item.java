91ffa5c6d3918cd27eae174e193068ac0c9d61af
bradencarthron <80701392+bradencarthron@users.noreply.github.com>
2024-04-19 19:20:27 -0400

    some changes to order

--- demo/src/main/java/com/example/Item.java
+++ demo/src/main/java/com/example/Item.java
@@ -4,13 +4,14 @@ public class Item {
     private int itemID;
     private String itemName;
     private double price;
+    private int quantity;
 
-
-    public Item(int itemID, String itemName, double price)
+    public Item(int itemID, String itemName, double price, int quantity)
     {
         this.itemID = itemID;
         this.itemName = itemName;
         this.price = price;
+        this.quantity = quantity;
     }
 
     public int getItemID() {
@@ -21,5 +22,8 @@ public class Item {
     }
     public double getPrice() {
         return price;
+    }
+    public int getQuantity() {
+        return quantity;
     }
 }
--- demo/src/main/java/com/example/Order.java
+++ demo/src/main/java/com/example/Order.java
@@ -12,6 +12,8 @@ public class Order {
     public Order(int orderID)
     {
         this.orderID = orderID;
+        itemslist = new LinkedList<Item>();
 
+        
     }
 }
