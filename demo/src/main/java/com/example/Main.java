aec51e0177a5865cb7fa48cf0ac1b645fc7c70e7
Jack Lusk <LandonLusk1@gmail.com>
2024-04-19 17:50:46 -0400

    Example changes

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -34,6 +34,7 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch();
         System.out.println("Hello World!");
+        System.out.println("AAAAAAAA");
     }
 
 }
