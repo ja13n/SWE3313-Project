596ed69a0314faf966eb7e39fd97708942e043af
bradencarthron <80701392+bradencarthron@users.noreply.github.com>
2024-04-21 15:15:10 -0400

    finish order class, changed list to array list and removed integer from argument of list

--- demo/src/main/java/com/example/Order.java
+++ demo/src/main/java/com/example/Order.java
@@ -1,19 +1,47 @@
 package com.example;
 
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class Order {
 
     private int orderID;
-    private LinkedList<Item> itemslist;
+    private ArrayList<Item> itemsList;
     
 
     public Order(int orderID)
     {
         this.orderID = orderID;
-        itemslist = new LinkedList<Item>();
-
-        
+        itemsList = new ArrayList<Item>();
     }
+
+    public void addItem(Item item)
+    {
+        itemsList.add(item);
+    }
+
+    public void removeItem(Item item)
+    {
+        itemsList.remove(item);
+    }
+
+    public ArrayList<Item> getItemsList() 
+    {
+        return itemsList;
+    }
+
+    public int getOrderID() 
+    {
+        return orderID;
+    }
+
+    public double calculateTotalPrice()
+    {
+        double total = 0;
+        for (int i = 0; i < itemsList.size(); i++) {
+            total += itemsList.get(i).getPrice();
+        }
+        return total;
+    }
+
 }
