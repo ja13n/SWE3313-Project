168391f1a822b7a8a77a60490934d0f076fd8961
joelk22 <joelmkim382001@gmail.com>
2024-04-24 14:55:49 -0400

    Added toString for customer and appropriate variables

--- demo/src/main/java/com/example/Customer.java
+++ demo/src/main/java/com/example/Customer.java
@@ -1,4 +1,5 @@
 package com.example;
+
 /**
  * Business object class for the customer.
  * @author ja13n
@@ -7,36 +8,41 @@ package com.example;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class Customer{
+public class Customer {
 
     private int customerID; // ID of the customer
-    private String companyName; // Company name 
+    private String companyName; // Company name
     private ContactInformation contactInfo; // contact information
     private boolean loadingDock; // Whether loading doc is available to customer
     private String deliveryHours; // Hours in which deliveries are accepted
-
+    private String email;
+    private String phone;
 
     /**
-     * Customer contructor class 
-     * @param customerID, holds the ID of the customer.
-     * @param companyName, holds he name of the company.
-     * @param contactinfo, holds the customer contact information.
-     * @param loadingDock, contains whether or not the customers order is in the loading dock.
+     * Customer contructor class
+     * 
+     * @param customerID,    holds the ID of the customer.
+     * @param companyName,   holds he name of the company.
+     * @param contactinfo,   holds the customer contact information.
+     * @param loadingDock,   contains whether or not the customers order is in the
+     *                       loading dock.
      * @param deliveryHours, contains the hours of delivery.
      **/
-    public Customer(int customerID, String companyName, ContactInformation contactinfo, boolean loadingDock, String deliveryHours) {
+    public Customer(int customerID, String companyName, ContactInformation contactinfo, boolean loadingDock,
+            String deliveryHours) {
         this.customerID = customerID;
         this.companyName = companyName;
         this.contactInfo = contactinfo;
         this.loadingDock = loadingDock;
         this.deliveryHours = deliveryHours;
-        
+        this.email = contactinfo.getEmail();
+        this.phone = contactinfo.getPhoneNumber();
     }
 
     /*
-     * Empty customer class. 
+     * Empty customer class.
      */
-    public Customer(){
+    public Customer() {
 
     }
 
@@ -59,33 +65,51 @@ public class Customer{
 
     /**
      * Method that returns customerID
+     * 
      * @return customerID
      */
-    public int getCustomerID() {return this.customerID;}
+    public int getCustomerID() {
+        return this.customerID;
+    }
 
     /**
      * Method that returns companyName
+     * 
      * @return companyName
      */
-    public String getCompanyName() {return this.companyName;}
+    public String getCompanyName() {
+        return this.companyName;
+    }
 
     /**
      * Method that returns the customers contact information.
+     * 
      * @return contactInfo
      */
-    public ContactInformation getContactInfo() {return this.contactInfo;}
+    public ContactInformation getContactInfo() {
+        return this.contactInfo;
+    }
 
     /**
      * Method returns true or false if customer order is in the loading dock.
+     * 
      * @return loadingDock
      */
-    public boolean hasLoadingDock() {return this.loadingDock;}
+    public boolean hasLoadingDock() {
+        return this.loadingDock;
+    }
 
     /**
      * Method that returns the delivery hours.
+     * 
      * @return deliveryHours
      */
-    public String getDeliveryHours() {return this.deliveryHours;}
+    public String getDeliveryHours() {
+        return this.deliveryHours;
+    }
 
-    
+    @Override
+    public String toString() {
+        return customerID + ", " + companyName + ", " + email + ", " + phone;
+    }
 }
