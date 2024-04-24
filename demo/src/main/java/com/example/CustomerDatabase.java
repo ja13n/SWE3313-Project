3899fa8a8355c95821d33179b1c4c078a8d8b1d1
joelk22 <joelmkim382001@gmail.com>
2024-04-24 14:47:45 -0400

    Added getCustomerId() method and variable

--- demo/src/main/java/com/example/CustomerDatabase.java
+++ demo/src/main/java/com/example/CustomerDatabase.java
@@ -2,48 +2,47 @@ package com.example;
 
 import java.util.ArrayList;
 
-public class CustomerDatabase
-{
+public class CustomerDatabase {
     private ArrayList<Customer> listOfCustomers;
-    
-    public CustomerDatabase()
-    {
+    private int nextCustomerId;
+
+    public CustomerDatabase() {
         listOfCustomers = new ArrayList<Customer>();
     }
 
-
-    public void addCustomer(Customer customer)
-    {
+    public void addCustomer(Customer customer) {
         listOfCustomers.add(customer);
+        nextCustomerId++;
     }
 
-    public void addCustomer(int customerID, String companyName, boolean loadingDock, String deliveryHours, String email, String phoneNumber)
-    {
+    public void addCustomer(int customerID, String companyName, boolean loadingDock, String deliveryHours, String email,
+            String phoneNumber) {
         ContactInformation contactinfo = new ContactInformation(email, phoneNumber);
         Customer customer = new Customer(customerID, companyName, contactinfo, loadingDock, deliveryHours);
         addCustomer(customer);
     }
-   
-    public void removeCustomer(Customer customer)
-    {
+
+    public void removeCustomer(Customer customer) {
         listOfCustomers.remove(customer);
     }
-    
-    public ArrayList<Customer> getlistofCustomers()
-    {
+
+    public ArrayList<Customer> getlistofCustomers() {
         return listOfCustomers;
     }
 
-    public String printlistofCustomers()
-    {
+    public String printlistofCustomers() {
         String rtn = "";
-        for(Customer customer : listOfCustomers)
-        {
-            rtn += customer.getCompanyName() + " " + customer.getDeliveryHours() + " " + customer.getContactInfo().getEmail() + " " + customer.getContactInfo().getPhoneNumber()
-             + "\n";
-             
+        for (Customer customer : listOfCustomers) {
+            rtn += customer.getCompanyName() + " " + customer.getDeliveryHours() + " "
+                    + customer.getContactInfo().getEmail() + " " + customer.getContactInfo().getPhoneNumber()
+                    + "\n";
+
         }
         return rtn;
+    }
+
+    public int getNextCustomerId() {
+        return nextCustomerId;
     }
 
 }
